d5122a7dcb0b80fa0f43d8115abf90ac858bc8ab
Feleuxens <felixgrueb@icloud.com>
2021-05-24 21:14:25 +0200

    Add default argument

--- test/mocks/message.py
+++ test/mocks/message.py
@@ -2,6 +2,6 @@ from test.mocks.channel import MockChannel
 
 
 class MockMessage:
-    def __init__(self, channel: MockChannel):
+    def __init__(self, channel: MockChannel = MockChannel()):
         self.channel = channel
         self.guild = channel.guild

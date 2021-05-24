83e755ea367001048b8ab66f1426acb6d8ea9a35
Feleuxens <felixgrueb@icloud.com>
2021-05-24 21:15:03 +0200

    Add bot methods for extensions

--- test/mocks/bot.py
+++ test/mocks/bot.py
@@ -4,3 +4,18 @@ from test.mocks.user import MockUser
 class MockBot:
     def __init__(self):
         self.user = MockUser()
+        self.loaded_extensions = []
+        self.cogs = []
+
+    def load_extension(self, extension_name: str):
+        self.loaded_extensions.append(extension_name)
+        return
+
+    def unload_extension(self, extension_name: str):
+        self.loaded_extensions.remove(extension_name)
+        return
+
+    def reload_extension(self, extension_name: str):
+        self.loaded_extensions.remove(extension_name)
+        self.loaded_extensions.append(extension_name)
+        return

856e2ca90a08a1416bc6e2d7cc9a9d75ae17d236
Feleuxens <felixgrueb@icloud.com>
2021-05-24 21:18:00 +0200

    Add check prefix test

--- test/test_main.py
+++ test/test_main.py
@@ -2,16 +2,25 @@ import pytest
 
 from test.mocks.bot import MockBot
 from test.mocks.channel import MockChannel
+from test.mocks.guild import MockGuild
 from test.mocks.message import MockMessage
 
 from dimabot.main import _check_prefix
 
 
-@pytest.mark.asyncio
-async def test_check_prefix():
-    bot = MockBot()
-    msg = MockMessage(channel=MockChannel())
+# noinspection PyTypeChecker
+class TestCheckPrefix:
+    @pytest.mark.asyncio
+    async def test_prefix_without_guild(self):
+        bot = MockBot()
+        msg = MockMessage()
+        assert await _check_prefix(bot, msg) == [f"<@!{bot.user.id}> ", f"<@{bot.user.id}> ", f"<@!{bot.user.id}>",
+                                                 f"<@{bot.user.id}>", "."]
 
-    # noinspection PyTypeChecker
-    assert await _check_prefix(bot, msg) == [f"<@!{bot.user.id}> ", f"<@{bot.user.id}> ", f"<@!{bot.user.id}>",
-                                             f"<@{bot.user.id}>", "."]
+    @pytest.mark.asyncio
+    async def test_prefix_with_unknown_guild(self):
+        bot = MockBot()
+        msg = MockMessage(channel=MockChannel(guild=MockGuild(123)))
+        assert await _check_prefix(bot, msg) == [f"<@!{bot.user.id}> ", f"<@{bot.user.id}> ", f"<@!{bot.user.id}>",
+                                                 f"<@{bot.user.id}>", "."]
+
